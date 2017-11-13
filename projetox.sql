0a92c312ee6d0916ecc726b8aebef133ec576e47
Marcelo Martins <marcelo@ufrgs.br>
2017-11-13 00:15:55 -0200

    Atualiza banco de dados e remove imagens

--- projetox.sql
+++ projetox.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 24-Out-2017 às 10:08
--- Versão do servidor: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: 13-Nov-2017 às 02:14
+-- Versão do servidor: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `projetox`
 --
-CREATE DATABASE IF NOT EXISTS `projetox` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `projetox`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +28,7 @@ USE `projetox`;
 -- Estrutura da tabela `lot`
 --
 
+DROP TABLE IF EXISTS `lot`;
 CREATE TABLE IF NOT EXISTS `lot` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `amount` int(10) UNSIGNED NOT NULL,
@@ -39,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `lot` (
   `is_activated` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `creator` (`creator`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,6 +48,7 @@ CREATE TABLE IF NOT EXISTS `lot` (
 -- Estrutura da tabela `password`
 --
 
+DROP TABLE IF EXISTS `password`;
 CREATE TABLE IF NOT EXISTS `password` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `iterations` int(10) UNSIGNED NOT NULL,
@@ -54,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `password` (
   `salt` varchar(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,6 +64,7 @@ CREATE TABLE IF NOT EXISTS `password` (
 -- Estrutura da tabela `reset`
 --
 
+DROP TABLE IF EXISTS `reset`;
 CREATE TABLE IF NOT EXISTS `reset` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `iterations` int(10) UNSIGNED NOT NULL,
@@ -73,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `reset` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user`),
   KEY `user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -81,6 +84,7 @@ CREATE TABLE IF NOT EXISTS `reset` (
 -- Estrutura da tabela `ticket`
 --
 
+DROP TABLE IF EXISTS `ticket`;
 CREATE TABLE IF NOT EXISTS `ticket` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `lot` int(10) UNSIGNED NOT NULL,
@@ -93,10 +97,11 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `password` varchar(64) NOT NULL,
   `salt` varchar(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `validated` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `seller_2` (`seller`),
   KEY `lot` (`lot`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -104,6 +109,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
 -- Estrutura da tabela `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -116,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `cpf` (`cpf`),
   KEY `password` (`password`),
   KEY `PASSWORD_ID` (`password`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Constraints for dumped tables
@@ -146,6 +152,7 @@ ALTER TABLE `ticket`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_password` FOREIGN KEY (`password`) REFERENCES `password` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
